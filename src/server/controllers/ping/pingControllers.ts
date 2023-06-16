97f3ca6743c34400182817534aa356658666a8c2
Mario González <mario@mariogl.com>
2023-06-16 08:16:51 +0200

    Add date to ping endpoint

--- src/server/controllers/ping/pingControllers.ts
+++ src/server/controllers/ping/pingControllers.ts
@@ -1,5 +1,8 @@
 import { type Request, type Response } from "express";
 
 export const getPong = (req: Request, res: Response) => {
-  res.status(200).json({ message: "OK" });
+  const today = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+
+  res.status(200).json({ message: "OK", time: today });
 };
