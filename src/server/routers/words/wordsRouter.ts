38c7f39088d2bff526917b29180248f8ff59ba34
Mario González <mario@mariogl.com>
2023-07-01 18:05:49 +0200

    Create endpoint POST /words

--- src/server/routers/words/wordsRouter.ts
+++ src/server/routers/words/wordsRouter.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import { validate } from "express-validation";
 import WordsMongoRepository from "../../../repositories/words/WordsMongoRepository.js";
-import { getWords } from "../../controllers/words/wordsControllers.js";
+import { addWord, getWords } from "../../controllers/words/wordsControllers.js";
 import { authMiddleware } from "../../middlewares/authMiddleware.js";
+import { newWordSchema } from "../../schemas/wordSchemas.js";
 
 const wordsRepository = new WordsMongoRepository();
 
 const wordsRouter = Router();
 
 wordsRouter.get("/", authMiddleware, getWords(wordsRepository));
+wordsRouter.post(
+  "/",
+  validate(newWordSchema, {}, { abortEarly: false }),
+  addWord(wordsRepository)
+);
 
 export default wordsRouter;
